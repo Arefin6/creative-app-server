dbc18d33dbd11fecbca249a5ea7b86c3a7fade3f
Arefi6 <arefinhossain3@gmail.com>
2020-10-17 17:36:25 +0600

    all post api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const fileUpload = require('express-fileupload');
+ const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 const objectId = require('mongodb').ObjectID;
 require('dotenv').config();
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('creative'));
+app.use(fileUpload());
 
 const port = 5000;
 
@@ -23,6 +25,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const ordersCollection = client.db("creativeAgency").collection("orders");
   const adminsCollection = client.db("creativeAgency").collection("admins");
+  const reviewsCollection = client.db("creativeAgency").collection("reviews");
+  const servicesCollection = client.db("creativeAgency").collection("services");
 
    app.post('/addAdmin',(req,res) =>{
       const adminEmail =req.body;
@@ -31,14 +35,68 @@ client.connect(err => {
           res.send(result.insertedCount>0);
       })
    })
-   app.post('/isAdmin',(req,res) =>{
-    const email =req.body.email;
-    adminsCollection.find({email:email})
-    .toArray((err,admins)=>{
-        res.send(admins.length>0);
+    app.post('/addReview',(req,res) =>{
+        const review =req.body;
+        reviewsCollection.insertOne(review)
+        .then(result =>{
+            res.send(result.insertedCount>0);
+        })
+    });
+
+    app.post('/addOrder',(req,res) =>{
+        const file = req.files.file;
+        const name = req.body.name;
+        const email = req.body.email;
+        const price = req.body.price;
+        const description = req.body.description;
+        const serviceName = req.body.serviceName;
+        
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        ordersCollection.insertOne({ name, email, image,price,description,serviceName })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+     });
+
+
+     app.post('/addService',(req,res) =>{
+        const file = req.files.file;
+        const title = req.body.title;
+        const description = req.body.description;
+        
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        servicesCollection.insertOne({ title,description,image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
     })
- });
- 
+
+
+
+    app.post('/isAdmin',(req,res) =>{
+        const email =req.body.email;
+        adminsCollection.find({email:email})
+        .toArray((err,admins)=>{
+            res.send(admins.length>0);
+        })
+    });
+    
 });
 
 
